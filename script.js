489a37fd1283614ac9ea8ef800b7c36e27dc45f1
Robert Chrobak <26276474+robertchrobak@users.noreply.github.com>
2018-02-26 02:53:03 -0500

    Changed reference to external font and reformatted

--- script.js
+++ script.js
@@ -3,23 +3,19 @@ var tipAmountSlide = document.getElementById("tipAmountSlide");
 var totalTip = document.getElementById("totalTip");
 var grandTotal = document.getElementById("grandTotal");
 
-
 submitButton.onclick = function() {
   subTotalValue = document.getElementById("subTotal").value;
   tipAmountValue = document.getElementById("tipAmountSlide").value;
-  var tipAmountDecimal = tipAmountValue/100;
+  var tipAmountDecimal = tipAmountValue / 100;
   console.log("BUTTON WORKS");
   calculateTip(Number(subTotalValue), tipAmountDecimal);
 }
-
 
 function calculateTip(subTotalValue, tipAmountDecimal) {
   var tipAmountFinal = subTotalValue * tipAmountDecimal;
   console.log(tipAmountFinal);
   var totalCheckAmount = tipAmountFinal + subTotalValue;
   console.log(totalCheckAmount);
-
   totalTip.innerHTML = "Tip Total: 💲" + tipAmountFinal.toFixed(2);
   grandTotal.innerHTML = "Check Total: 💲" + totalCheckAmount.toFixed(2);
-
 }
